875573542e1381260831d9b5bbc15459ea765da5
John Howard (VM) <jhoward@ntdev.microsoft.com>
2017-03-09 12:09:43 -0800

    Minor
    Signed-off-by: John Howard (VM) <jhoward@ntdev.microsoft.com>

--- PSRemote-RS1CIServer.ps1
+++ PSRemote-RS1CIServer.ps1
@@ -1,12 +1,10 @@
-﻿#http://stackoverflow.com/questions/22233702/how-to-download-the-ssl-certificate-from-a-website-using-powershell
-
-
-$nodeID = Read-Host -Prompt 'Server node ID (eg 99 for jenkins-rs1-99)'
+﻿$nodeID = Read-Host -Prompt 'Server node ID (eg 99 for jenkins-rs1-99)'
 $vmName="jenkins-rs1-$nodeID"
 $vm = Get-AzureVM $vmName
 $ep = Get-AzureEndpoint -vm $vm | Where-Object { $_.Name -eq 'Powershell' }
 $dest="`'@{TrustedHosts=`"$($ep.vip)`"}`'"
 
+#http://stackoverflow.com/questions/22233702/how-to-download-the-ssl-certificate-from-a-website-using-powershell
 $url="https://$vmName.cloudapp.net:$($ep.Port)"
 $webRequest = [Net.WebRequest]::Create($url)
 try { $webRequest.GetResponse() } catch {}
@@ -24,7 +22,7 @@ $cred=New-Object System.Management.Automation.PSCredential("jenkins", $secPass)
 winrm s winrm/config/client '@{TrustedHosts="jenkins-rs1-1.cloudapp.net"}'
 Enter-pssession -ComputerName "$vmName.cloudapp.net" -UseSSL -Port $($ep.Port) -Credential $cred
 
-#winrm s winrm/config/client $dest
+
 
 
 
